1e2d3ecf86241f796304ac7fe911f49022316914
Diego Mais <diegomais@live.com>
2019-08-08 22:41:42 -0300

    Add root import
    - Simplify import paths in project.
    
    On branch master
    Changes to be committed:
    	modified:   .eslintrc.js
    	modified:   babel.config.js
    	new file:   jsconfig.json
    	modified:   package.json
    	modified:   yarn.lock

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,4 +33,11 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
