d57b8d1446901ea5581a37629454b4aafbb9f93a
Diego Mais <diegomais@live.com>
2019-08-11 07:53:33 -0300

    Add StatusBar
    - This Component control the app status bar .
    
    On branch master
    Changes to be committed:
    	modified:   src/components/Input/index.js
    	modified:   src/index.js

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,7 +7,7 @@ import { Container, TInput } from './styles';
 function Input({ style, icon, ...rest }, ref) {
   return (
     <Container style={style}>
-      {icon && <Icon name={icon} size={20} color="rgba(255,255,255,0.6)" />}
+      {icon && <Icon name={icon} size={20} color="rgba(255, 255, 255, 0.6)" />}
       <TInput {...rest} ref={ref} />
     </Container>
   );
