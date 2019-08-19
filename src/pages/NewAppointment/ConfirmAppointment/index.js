4c0ee61ecb902462f0d942027d628429143df5c0
Diego Mais <diegomais@live.com>
2019-08-19 00:14:38 -0300

    Add confirm appointment page
    - This page provides user do confirm information to create a new appointment.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/NewAppointment/ConfirmAppointment/index.js
    	new file:   src/pages/NewAppointment/ConfirmAppointment/styles.js

--- src/pages/NewAppointment/ConfirmAppointment/index.js
+++ src/pages/NewAppointment/ConfirmAppointment/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-export default function ConfirmAppointment() {
-  return <View />;
+import Background from '~/components/Background';
+
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
+
+export default function ConfirmAppointment({ navigation }) {
+  const provider = navigation.getParam('provider');
+  const time = navigation.getParam('time');
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date()),
+    [time]
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('Dashboard');
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+        <Name>{provider.name}</Name>
+        <Time>{dateFormatted}</Time>
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirm Appointment
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
+
+ConfirmAppointment.navigationOptions = ({ navigation }) => ({
+  title: 'Confirm Appointment',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack();
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
+
+ConfirmAppointment.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+    getParam: PropTypes.func.isRequired,
+  }).isRequired,
+};
