988a9031f256755d0249410ebbdc5850ff37a64f
Diego Mais <diegomais.dev@gmail.com>
2020-04-22 20:45:18 -0300

    WIP(api): Add appointments routes The route must receive provider and date within the request body. When registering a new appointment, it must be stored within an array of objects.

---
+++ api/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import appointmentsRouter from './appointments.routes';
+
+const routes = Router();
+
+routes.use('/appointments', appointmentsRouter);
+
+export default routes;
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'GoBarber' })
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
