cbf37c2e3dd76052a9a3b45b98f1a91424342515
Diego Mais <diegomais.dev@gmail.com>
2020-04-22 20:55:53 -0300

    WIP(api): Add appointments routes The route must reject appointments in same time.

--- api/src/routes/appointments.routes.ts
+++ api/src/routes/appointments.routes.ts
@@ -1,14 +1,33 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = { id: uuid(), provider, date };
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameTime = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameTime) {
+    return response
+      .status(400)
+      .json({ message: 'This time is already booked.' });
+  }
+
+  const appointment = { id: uuid(), provider, date: parsedDate };
 
   appointments.push(appointment);
 
