fe4a7449ee5b2a59fe38351cdb6b04549b5a0354
Diego Mais <diegomais.dev@gmail.com>
2020-04-22 21:06:07 -0300

    WIP(api): Add Appointment model Abstract Appointment interface into a model.

--- api/src/routes/appointments.routes.ts
+++ api/src/routes/appointments.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -27,7 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This time is already booked.' });
   }
 
-  const appointment = { id: uuid(), provider, date: parsedDate };
+  const appointment = new Appointment({ provider, date: parsedDate });
 
   appointments.push(appointment);
 
