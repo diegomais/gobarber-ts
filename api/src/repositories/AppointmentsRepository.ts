3a964ee94e0e21e1813227c3f76fea0a97f6f314
Diego Mais <diegomais.dev@gmail.com>
2020-04-22 21:33:28 -0300

    feat(api): Add list appointments GET /appointments: This route returns a list with all the registered appointments.

--- api/src/repositories/AppointmentsRepository.ts
+++ api/src/repositories/AppointmentsRepository.ts
@@ -9,6 +9,10 @@ class AppointmentsRepository {
     this.appointments = [];
   }
 
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment =>
       isEqual(date, appointment.date),
--- api/src/routes/appointments.routes.ts
+++ api/src/routes/appointments.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
+
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
 const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
