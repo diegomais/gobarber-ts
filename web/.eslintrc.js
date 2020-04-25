f0347b47f5cec10f22080ba6b8d3bf003544c758
Diego Mais <diegomais.dev@gmail.com>
2020-04-25 11:02:46 -0300

    style(web): Add EditorConfig, ESLint and Prettier EditorConfig helps maintain consistent coding styles for multiple developers working on the same project across various editors and IDEs. The EditorConfig project consists of a file format for defining coding styles and a collection of text editor plugins that enable editors to read the file format and adhere to defined styles. ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs. Prettier is an opinionated code formatter.

---
+++ web/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint', 'prettier'
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { 'extensions': ['.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        'allowExpressions': true
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'ts': 'never',
+        'tsx': 'never'
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      'typescript': {}
+    }
+  }
+};
