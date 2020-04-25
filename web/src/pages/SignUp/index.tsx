cb651d4b5929199fbbb69979f3fe1e9d3124f7ec
Diego Mais <diegomais.dev@gmail.com>
2020-04-25 16:59:57 -0300

    WIP(web): Add input usability User should receive feedback when focus and blur from an input.

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
 
 import Input from '../../components/Input';
@@ -7,36 +8,42 @@ import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+  return (
+    <Container>
+      <Background />
 
-      <form>
-        <h1>Sign up</h1>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Input name="name" icon={FiUser} placeholder="Name" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Sign up</h1>
 
-        <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input name="name" icon={FiUser} placeholder="Name" />
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Password"
-        />
+          <Input name="email" icon={FiMail} placeholder="Email" />
 
-        <Button type="submit">Sign up</Button>
-      </form>
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
 
-      <a href="?">
-        <FiArrowLeft />
-        Log in
-      </a>
-    </Content>
-  </Container>
-);
+          <Button type="submit">Sign up</Button>
+        </Form>
+
+        <a href="?">
+          <FiArrowLeft />
+          Log in
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
