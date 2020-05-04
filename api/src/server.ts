ba3feb1429c02e53b2df7d893c066148d1f7572d
Diego Mais <diegomais.dev@gmail.com>
2020-05-03 21:28:30 -0300

    chore(api): Add CORS and enable all CORS requests CORS is a node.js package for providing a Express middleware that can be used to enable CORS with various options.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import './database';
@@ -9,6 +10,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
