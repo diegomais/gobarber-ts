41bee6f353d5d732f0f02754b76e2cfe4fba05cc
Diego Mais <diegomais.dev@gmail.com>
2020-09-27 21:20:36 -0300

    ✅ api: add UpdateUserAvatarService tests
    FakeStorageProvider was created to replace disk storage during the tests.

---
+++ api/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,74 @@
+import AppError from '@shared/errors/AppError';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatarService', () => {
+  it('should be able to update user avatar', async () => {
+    const fakeStorageProvider = new FakeStorageProvider();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'email@domain.com',
+      password: 'MySecretPassword',
+    });
+
+    const avatarFilename = 'avatar.jpg';
+    await updateUserAvatarService.execute({ user_id: user.id, avatarFilename });
+
+    expect(user.avatar).toBe(avatarFilename);
+  });
+
+  it('should not be able to update avatar of an unauthenticated user', async () => {
+    const fakeStorageProvider = new FakeStorageProvider();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatarService.execute({
+        user_id: 'unauthenticated-user',
+        avatarFilename: 'avatar.jpg',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to update user avatar', async () => {
+    const fakeStorageProvider = new FakeStorageProvider();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'email@domain.com',
+      password: 'MySecretPassword',
+    });
+
+    const oldAvatar = 'avatar.jpg';
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: oldAvatar,
+    });
+
+    const newAvatar = 'new-avatar.jpg';
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: newAvatar,
+    });
+
+    expect(deleteFile).toBeCalledWith(oldAvatar);
+    expect(user.avatar).toBe(newAvatar);
+  });
+});
---
+++ api/src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -0,0 +1,19 @@
+import IStorageProvider from '../models/IStorageProvider';
+
+class FakeStorageProvider implements IStorageProvider {
+  private storage: string[] = [];
+
+  public async saveFile(file: string): Promise<string> {
+    this.storage.push(file);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const findIndex = this.storage.findIndex(item => item === file);
+
+    this.storage.splice(findIndex, 1);
+  }
+}
+
+export default FakeStorageProvider;
