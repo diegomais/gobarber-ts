d3514ddecfa67a282aaa2cb13117ebcb25ea982c
Diego Mais <diegomais.dev@gmail.com>
2020-10-02 19:44:11 -0300

    🔧 api: add UserPasswordRecoveryTokensRepository
    Add UserPasswordRecoveryTokensRepositoryinto Inversion of Control Container.

--- api/src/shared/container/index.ts
+++ api/src/shared/container/index.ts
@@ -6,12 +6,20 @@ import './providers';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
+import IUserPasswordRecoveryTokensRepository from '@modules/users/repositories/IUserPasswordRecoveryTokensRepository';
+import UserPasswordRecoveryTokensRepository from '@modules/users/infra/typeorm/repositories/UserPasswordRecoveryTokensRepository';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
   AppointmentsRepository,
+);
+
+container.registerSingleton<IUserPasswordRecoveryTokensRepository>(
+  'UserPasswordRecoveryTokensRepository',
+  UserPasswordRecoveryTokensRepository,
 );
 
 container.registerSingleton<IUsersRepository>(
