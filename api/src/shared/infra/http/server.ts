aa57a251fcf7cac115fb5a59c29c5fd37e72a3d7
Diego Mais <diegomais.dev@gmail.com>
2020-10-02 20:15:22 -0300

    🔧 api: update uploads folder on Express static

--- api/src/shared/infra/http/server.ts
+++ api/src/shared/infra/http/server.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
