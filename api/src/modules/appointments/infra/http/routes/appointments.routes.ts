37c65bb9a7afed927da796e55ec39572818908a7
Diego Mais <diegomais.dev@gmail.com>
2020-12-06 22:09:15 -0300

    🐛 (api): set date as required to create appointment

--- api/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ api/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -16,7 +16,7 @@ appointmentsRouter.post(
   celebrate({
     [Segments.BODY]: {
       provider_id: Joi.string().uuid().required(),
-      date: Joi.date(),
+      date: Joi.date().required(),
     },
   }),
   appointmentsController.store,
