8884c60dd0533d0d007f485364f858547607b885
Diego Mais <diegomais.dev@gmail.com>
2020-12-06 22:17:32 -0300

    ✨ (api): add customer data to provider's schedule

--- api/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ api/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -45,6 +45,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDate}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['customer'],
     });
 
     return appointments;
