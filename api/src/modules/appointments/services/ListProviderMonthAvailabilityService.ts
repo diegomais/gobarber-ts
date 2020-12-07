cd7f41281bf8793cb7a696d33a90ebbed7786bc7
Diego Mais <diegomais.dev@gmail.com>
2020-12-06 21:42:55 -0300

    🐛 (api): set past dates as unavailable

--- api/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ api/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -38,11 +38,17 @@ class ListProviderMonthAvailabilityService {
     const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
 
     const availability = days.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(
         appointment => getDate(appointment.date) === day,
       );
 
-      return { day, available: appointmentsInDay.length < 10 };
+      return {
+        day,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
+      };
     });
 
     return availability;
